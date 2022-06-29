94f6e57daa6519f23027249963315e6287f5fb52
fgiardin <67904874+fgiardin@users.noreply.github.com>
2022-06-29 14:53:00 +0200

    changed suppl fig 5- now fig. 5 in main text

--- analysis/4.other_figures/print_fET_vs_CWD_allsites_bygroup.R
+++ analysis/4.other_figures/print_fET_vs_CWD_allsites_bygroup.R
@@ -12,6 +12,11 @@ b <- heatscatter(x=df$deficit, y = df$fvar, ggplot = TRUE)
 b$data$name_site = df$name_site   # hack the name of the site back into the ggplot object (it won't take it from original df through heatscatter function)
 b <- b + labs(y = "fET (-)", x = "Cumulative water deficit (mm)") +
   theme_classic() +
+  theme(
+    axis.text=element_text(size = 17),
+    axis.title=element_text(size = 19),
+    strip.text = element_text(size=17) # plot title of facet_wrap multiplot
+  ) +
   scale_y_continuous(breaks = seq(0, 1.4, 0.4), limits = c(0, 1.4)) +
   scale_x_continuous(breaks = seq(0, 300, 100), limits = c(0, 300)) +  #
   facet_wrap(~name_site, ncol = 4)
@@ -20,8 +25,8 @@ b
 # save figure
 ggsave("facet_highET.png",
        path = "./output",
-       width = 5,
-       height = 5,
+       width = 10,  # by increasing ratio here, the points on final figure will appear smaller/bigger (i.e. 10-12 will yield smaller points than 5-6)
+       height = 10,
 )
 
 
@@ -32,16 +37,22 @@ b <- heatscatter(x=df$deficit, y = df$fvar, pch = "6", ggplot = TRUE)
 b$data$name_site = df$name_site
 b <- b + labs(y = "fET (-)", x = "Cumulative water deficit (mm)") +
   theme_classic() +
+  theme(
+    axis.text=element_text(size = 17),
+    axis.title=element_text(size = 19),
+    strip.text = element_text(size=17) # plot title of facet_wrap multiplot
+    ) +
   scale_y_continuous(breaks = seq(0, 1.4, 0.4), limits = c(0, 1.4)) +
   scale_x_continuous(breaks = seq(0, 300, 100), limits = c(0, 300)) +
   facet_wrap(~name_site, ncol = 4)
 b
 
+
 # save figure
 ggsave("facet_mediumET.png",
        path = "./output",
-       width = 5,
-       height = 6,
+       width = 10,
+       height = 12,
 )
 
 
@@ -52,15 +63,20 @@ b <- heatscatter(x=df$deficit, y = df$fvar, pch = "6", ggplot = TRUE)
 b$data$name_site = df$name_site
 b <- b + labs(y = "fET (-)", x = "Cumulative water deficit (mm)") +
   theme_classic() +
+  theme(
+    axis.text=element_text(size = 17),
+    axis.title=element_text(size = 19),
+    strip.text = element_text(size=17) # plot title of facet_wrap multiplot
+  ) +
   scale_y_continuous(breaks = seq(0, 1.4, 0.4), limits = c(0, 1.4)) +
   scale_x_continuous(breaks = seq(0, 300, 100), limits = c(0, 300)) +  #
-  facet_wrap(~name_site, ncol = 4)
+  facet_wrap(~name_site, ncol = 3)
 b
 
 # save figure
 ggsave("facet_lowET.png",
        path = "./output",
-       width = 5,
-       height = 3,
+       width = 8,
+       height = 8,
 )
 
