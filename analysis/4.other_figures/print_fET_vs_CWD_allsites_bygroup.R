b2b31dc2141bacceb23b7bafc8b34cc69d39d65d
fgiardin <67904874+fgiardin@users.noreply.github.com>
2023-04-16 17:46:34 +0200

    added EVI

--- analysis/4.other_figures/print_fET_vs_CWD_allsites_bygroup.R
+++ analysis/4.other_figures/print_fET_vs_CWD_allsites_bygroup.R
@@ -41,13 +41,13 @@ plot_allsites_fvar <- plot_allsites_fvar %>%
 
 # remove outliers and scale EVI (not working really well)
 plot_allsites_EVI_raw <- plot_allsites_fvar %>%
-  na.omit() %>%
   group_by(name_site) %>%
+  #questa normalizzazione non cambia niente se plotto in second axis
   mutate(
     EVI_scaled = EVI / median(EVI[deficit < 20])) %>% # take the median of the lower bin of the deficit
-  filter(between(EVI_scaled,
-                 mean(EVI_scaled, na.rm=TRUE) - (2.5 * sd(EVI_scaled, na.rm=TRUE)), # remove outliers by site
-                 mean(EVI_scaled, na.rm=TRUE) + (2.5 * sd(EVI_scaled, na.rm=TRUE)))) %>%
+  # filter(between(EVI_scaled,
+  #                mean(EVI_scaled, na.rm=TRUE) - (2.5 * sd(EVI_scaled, na.rm=TRUE)), # remove outliers by site
+  #                mean(EVI_scaled, na.rm=TRUE) + (2.5 * sd(EVI_scaled, na.rm=TRUE)))) %>%
   mutate(bin = as.numeric(cut_number(deficit,{n()/50}))) %>% # dynamic number of bins: equal to
                                                                  # number of rows per PFT divided by 40
                                                                  # aka at least 40 points per bin
@@ -69,19 +69,21 @@ plot_allsites_EVI <- plot_allsites_EVI_raw %>%
 
 ### HIGH ET ####
 # get a list of high ET sites and plot them individually
-df <- plot_allsites_fvar %>% dplyr::filter(cluster == "high fET")
+df <- plot_allsites_EVI %>% dplyr::filter(cluster == "high fET")
 high_fET <- unique(df$name_site) # list of sites within fET group
 results_highET <- list() # initialize list to save plots
 
 for (i in 1:length(high_fET)){
   site <- high_fET[i]
 
-  df <- plot_allsites_fvar %>% dplyr::filter(name_site == site)
+  df <- plot_allsites_EVI %>% dplyr::filter(name_site == site)
   a <- heatscatter(x=df$deficit, y = df$fvar, ggplot = TRUE)
   a$data$name_site = df$name_site   # hack the name of the site back into the ggplot object (it won't take it from original df through heatscatter function)
   a <- a + labs(y = "fET (-)",
                 x = "CWD (mm)",
                 title = site) +
+    geom_ribbon(data = df, aes(x = medianCWD, ymin = perc_25, ymax = perc_75), fill = "#0CFA07", alpha=0.3) +
+    geom_line(data = df, aes(x = medianCWD, y = perc_50), color="#0CFA07", alpha=0.95, size = 1) +
     theme_classic() +
     theme(
       axis.text=element_text(size = 17),
@@ -94,11 +96,11 @@ for (i in 1:length(high_fET)){
   a
 
   results_highET[[length(results_highET) + 1]] <- a # store plot in a list
-  print(low_fET[i])
+  print(high_fET[i])
 }
 
 # functions to remove x and y axes when not needed
-remove_y <- theme(
+{remove_y <- theme(
   axis.text.y = element_blank(),
   axis.ticks.y = element_blank(),
   axis.title.y = element_blank()
@@ -128,7 +130,7 @@ results_highET[[12]] <- results_highET[[12]] + remove_x + remove_y
 results_highET[[13]] <- results_highET[[13]]
 results_highET[[14]] <- results_highET[[14]] + remove_y
 results_highET[[15]] <- results_highET[[15]] + remove_y
-results_highET[[16]] <- results_highET[[16]] + remove_y
+results_highET[[16]] <- results_highET[[16]] + remove_y}
 
 # create one plot
 wrap_plots(results_highET, ncol = 4, nrow = 4) + plot_layout(guides = "collect")
@@ -148,28 +150,27 @@ df <- plot_allsites_fvar %>% dplyr::filter(cluster == "medium fET")
 medium_fET <- unique(df$name_site) # list of sites within fET group
 results_mediumET <- list() # initialize list to save plots
 
+# df <- plot_allsites_fvar %>% dplyr::filter(name_site == site)
+
 for (i in 1:length(medium_fET)){
   # define site i
   site <- medium_fET[i]
 
-  site <- "AU-How"
-
-  df <- plot_allsites_fvar %>% dplyr::filter(name_site == site)
   df_EVI <- plot_allsites_EVI %>% dplyr::filter(name_site == site)
 
-  # calculate coefficient to scale two variables for visual comparison (so that maxima match)
-  coeff <- max(df_EVI$EVI_scaled, na.rm = TRUE)/max(df$fvar, na.rm = TRUE)
+  # # calculate coefficient to scale two variables for visual comparison (so that maxima match)
+  # coeff <- max(df_EVI$EVI_scaled, na.rm = TRUE)/max(df$fvar, na.rm = TRUE)
 
   # plot
-  b <- heatscatter(x=df$deficit, y = df$fvar, ggplot = TRUE)
-  b$data$name_site = df$name_site   # hack the name of the site back into the ggplot object (it won't take it from original df through heatscatter function)
+  b <- heatscatter(x=df_EVI$deficit, y = df_EVI$fvar, ggplot = TRUE)
+  b$data$name_site = df_EVI$name_site   # hack the name of the site back into the ggplot object (it won't take it from original df through heatscatter function)
 
   a <- b + labs(y = "fET (-)",
                 x = "CWD (mm)",
                 title = site) +
     theme_classic() +
-    #geom_point(data = df_EVI, aes(x = deficit, y = EVI_scaled/coeff), size=1.5, alpha = 0.5, color="#0a6228", position = position_stack(reverse = TRUE)) + # Divide by coeff to get the same range than other variable
     geom_ribbon(data = df_EVI, aes(x = medianCWD, ymin = perc_25, ymax = perc_75), fill = "#0CFA07", alpha=0.3) +
+    geom_line(data = df_EVI, aes(x = medianCWD, y = perc_50), color="#0CFA07", alpha=0.95, size = 1) +
     theme(
       axis.text=element_text(size = 17),
       axis.title=element_text(size = 19),
@@ -177,11 +178,11 @@ for (i in 1:length(medium_fET)){
       plot.title = element_text(hjust = 0.5, size = 19) # center title and change size
     ) +
     scale_y_continuous(
-      breaks = seq(0, 1.4, 0.4), limits = c(0, 1.4), expand = c(0, 0),
-      sec.axis = sec_axis(~.*coeff, name="EVI (-)")) # Add a second axis and specify its features
-    # scale_x_continuous(breaks = seq(0, 300, 100), limits = c(0, 325), expand = c(0, 0))
-  c <- ggplot() + geom_line(data = df_EVI, aes(x = medianCWD, y = perc_50), color="#0CFA07", alpha=0.95, size = 1)
-  c
+      breaks = seq(0, 1.4, 0.4), limits = c(0, 1.4), expand = c(0, 0)
+      # sec.axis = sec_axis(~.*coeff, name="EVI (-)") # Add a second axis and specify its features
+    )
+    scale_x_continuous(breaks = seq(0, 300, 100), limits = c(0, 325), expand = c(0, 0))
+  a
 
   results_mediumET[[length(results_mediumET) + 1]] <- a # store plot in a list
   print(medium_fET[i])
